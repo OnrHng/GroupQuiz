7c96a1f1e43552e2fb71698ca13177ba35b3627b
Onur Hangül <42890353+OnrHng@users.noreply.github.com>
2021-04-18 12:42:20 +0200

    Update configDb.js

--- backend/configDb.js
+++ backend/configDb.js
@@ -1,9 +1,9 @@
 // define db settings here
 const dbSettings =  {
-  host: "eu-cdbr-west-03.cleardb.net",
-  user: "b57010b57160f7",
-  password: "2a043de6",
-  database: "heroku_444912bf79fb7aa"
+  host: "yourHostName",
+  user: "Username",
+  password: "yourPsw",
+  database: "YourDBName"
 };
 
 module.exports.dbSettings = dbSettings;
